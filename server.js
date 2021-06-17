8f9c3a80bd49fb04a96c9df709781c24806d6e79
Howard <vault10howard@gmail.com>
2021-06-17 13:24:11 +0800

    commit to heroku with port change

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const {
 } = require('./middleware/auth_middleware')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // =====================================
 //          Using Middleware
